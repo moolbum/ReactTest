e48e901d90cc1e4167fa1669f8dc343e24265de4
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-01-27 01:43:48 +0900

    Add : 이미지,동영상 업로드 미리보기

--- src/pages/Main/Number/First.js
+++ src/pages/Main/Number/First.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const First = () => {
+  const [file, setFile] = useState({});
+
+  const imageUpload = e => {
+    const imageTpye = e.target.files[0].type.includes('image');
+    const videoTpye = e.target.files[0].type.includes('video');
+
+    setFile({
+      url: URL.createObjectURL(e.target.files[0]),
+      image: imageTpye,
+      video: videoTpye,
+    });
+    console.log(imageTpye);
+  };
+
   return (
     <Wrap>
       <h1>1번</h1>
+      <input type="file" onChange={imageUpload} />
+      {file.image && <img src={file.url} />}
+      {file.video && <video src={file.url} controls width="350px" />}
     </Wrap>
   );
 };
