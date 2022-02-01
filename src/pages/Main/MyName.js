2aa31cc3b5e53837677866ad0b638e84283cd870
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-02-01 21:34:49 +0900

    Modify : defaultProps 수정

--- src/pages/Main/MyName.js
+++ src/pages/Main/MyName.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MyName = ({ name }) => {
-  MyName.defaultProps = {
-    name: '기본이름',
-  };
   return (
     <div>
       <Text>안녕하세요 제 이름은 {name} 입니다.</Text>
     </div>
   );
+};
+
+MyName.defaultProps = {
+  name: '기본이름',
 };
 
 export default MyName;
